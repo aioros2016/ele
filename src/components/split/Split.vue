92bb0fa74488c497b5a8849b63913b63604e5225
aioros2016 <xiaozhishmily@aliyun.com>
2017-06-20 16:18:05 +0800

    样式修改

--- src/components/split/Split.vue
+++ src/components/split/Split.vue
@@ -1,9 +1,7 @@
 <template>
-	<div class="split">
-		
-	</div>
+	<div class="split"></div>
 </template>
 
 <style lang="less">
-	.split { border-top: rgba(7, 17, 27, .1); border-bottom: rgba(7, 17, 27, .1); width: 100%; height: 16px; background-color: #f3f5f7;}
+	.split { border-top: 1px solid rgba(7, 17, 27, .1); border-bottom: 1px solid rgba(7, 17, 27, .1); width: 100%; height: 16px; background-color: #f3f5f7;}
 </style>
